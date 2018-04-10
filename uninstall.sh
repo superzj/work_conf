364ad393a4b71581caf9cabc510e6852163a7464
superzj <zhangjian110518@gmail.com>
2018-04-10 17:15:19 +0800

    add unlink zsh

--- uninstall.sh
+++ uninstall.sh
@@ -4,9 +4,12 @@ pwd=$PWD
 
 home=$HOME
 
+zsh ./stand_uni.zsh
+
 unlink ${home}/.bash_profile
 unlink ${home}/.vim
 unlink ${home}/.vimrc
 unlink ${home}/.zprezto
 unlink ${home}/.autojump
 unlink ${home}/.tmux.conf
+
