8090f52c2fc646cfca1af800141e35194483ec7d
superzj <zhangjian110518@gmail.com>
2018-04-10 17:02:32 +0800

    modify ini and add uni

---
+++ uninstall.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env sh
+
+pwd=$PWD
+
+home=$HOME
+
+unlink ${home}/.bash_profile
+unlink ${home}/.vim
+unlink ${home}/.vimrc
+unlink ${home}/prezto
+unlink ${home}/.autojump
+unlink ${home}/.tmux.conf
